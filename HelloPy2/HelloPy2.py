c57743899a551d0e1744717d094fed33c0c1b2d0
Bob Setterbo <bob@mbiz.com>
2018-04-19 07:38:17 -0700

    Added boilerplate docstring and __main__ test

--- HelloPy2/HelloPy2.py
+++ HelloPy2/HelloPy2.py
@@ -1,1 +1,29 @@
-print("Hello Visual Studio from Python 2.7")
+#! /usr/bin/env python
+""" 
+     HelloPy2.py - Test python 2.7 install in new Visual Studio on new computer.
+          ToDo: Create a template to use for new files       
+"""
+__author__ = "Bob Setterbo (bob@thesetterbogroup.com)"
+__version__ = "$Revision: 1.0 $"
+__date__ = "$Date: 2018/4/17 15:42:10 $"
+__copyright__ = "Copyright (c) 2018 Bob Setterbo"
+__license__ = "Python"
+
+
+#=========================================================================== 
+def main():
+    """ 
+    Main - entry point for the script.
+    
+        HelloPy2.py 
+
+        Print out a welcome string
+s
+    """
+    print("Hello Visual Studio from Python 2.7")
+
+
+#=========================================================================== 
+if __name__ == '__main__':
+    main()
+   
